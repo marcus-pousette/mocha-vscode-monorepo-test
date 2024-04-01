33e116c34e78c12538106065e97f3c8a3d4c89f4
Marcus Pousette <marcus.pousette@quantleaf.com>
2024-04-01 22:23:17 +0200

    test: add top level await

--- packages/b/test/index.spec.ts
+++ packages/b/test/index.spec.ts
@@ -1,6 +1,9 @@
 import { expect } from 'chai'
 import { subtract } from '../src/index.js'
  
+// this seems to make the test explorer to not work anymore
+const fn = async () => {}
+await fn()
 
 describe("suite", () => {
     it('test', () => {
